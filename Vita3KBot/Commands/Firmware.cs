3c9d30acecc8774e0683566ad72ac73330634c5b
Hesham Abourgheba <hisham.abourgheba@gmail.com>
2020-08-18 00:23:03 +0300

    switch to github release (#13)
    * add license agreement for firmware
    
    * switch to github releases

--- Vita3KBot/Commands/Firmware.cs
+++ Vita3KBot/Commands/Firmware.cs
@@ -16,6 +16,7 @@ namespace Vita3KBot.Commands {
             .WithTitle($"The latest firmware version is {PSNClient.GetFWVersion()}")
             .WithColor(Color.Orange)
             .WithDescription("Installing the firmware packages in Vita3K allows the emulator to LLE the system modules.")
+            .AddField("License Agreement", "Before downloading the firmware you must read and agree to the license agreement located [here](https://doc.dl.playstation.net/doc/psvita-eula/)")
             .AddField("Modules Package", $"[Full Firmware Package ({PSNClient.GetFullFW().Item2}MB)]({PSNClient.GetFullFW().Item1})", true)
             .AddField("Fonts Package", $"[Systemdata Firmware Package ({PSNClient.GetSystemDataFW().Item2}MB)]({PSNClient.GetSystemDataFW().Item1})", true)
             .WithFooter("Both packages have to be installed in Vita3K in order for them to function properly")

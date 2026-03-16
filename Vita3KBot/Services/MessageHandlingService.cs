21652c4ad1dab54c2e911af4211067ba93669bb3
nishinji <nishinji@users.noreply.github.com>
2026-03-16 18:22:33 +0900

    Disable blacklist func

--- Vita3KBot/Services/MessageHandlingService.cs
+++ Vita3KBot/Services/MessageHandlingService.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 
 using Discord;
 using Discord.WebSocket;
-using Vita3KBot.Database;
 
 using Microsoft.Extensions.DependencyInjection;
 using APIClients;
@@ -13,29 +12,6 @@ namespace Vita3KBot.Services {
     public class MessageHandlingService {
         private readonly DiscordSocketClient _client;
         private readonly IServiceProvider _services;
-        
-        // Called for each user message. Use it to collect stats, or silently observe stuff, etc.
-        private static async Task MonitorMessage(SocketUserMessage message) {
-            if (!(message.Author is SocketGuildUser user) || message.Author.IsBot) return;
-            try {
-                using var db = new BotDb();
-                if (!RolesUtils.IsModerator(message.Author) && db.blacklistTerms.Any(term => message.Content.Contains(term.BlacklistedText))) {
-                    var embed = new EmbedBuilder()
-                    .WithTitle("Scam link detected and message deleted")
-                    .WithDescription(message.Content)
-                    .AddField("Sent by", message.Author.Mention)
-                    .AddField("In Channel", message.Channel)
-                    .WithColor(Color.Red)
-                    .Build();
-                    var channel = message.Channel as SocketGuildChannel;
-                    await channel.Guild.GetTextChannel(757604199159824385).SendMessageAsync(embed: embed).ConfigureAwait(false);
-                    await message.DeleteAsync().ConfigureAwait(false);
-                }
-            } catch(Exception ex) {
-                Console.WriteLine(ex);
-            }
-            //You can put some monitoring here.
-        }
 
         private static async Task MonitorNewBuilds(SocketUserMessage msg) {
             if (msg.Channel.Name != "github" && msg.Author.ToString() != "GitHub#0000") return;
@@ -74,7 +50,6 @@ namespace Vita3KBot.Services {
         private async Task CheckMessage(SocketMessage message) {
             if (!(message is SocketUserMessage userMessage)) return;
 
-            await MonitorMessage(userMessage);
             await MonitorNewBuilds(userMessage);
             await MonitorMediaMessages(userMessage);
         }
